9e0507c54538bbc8f60c36063109ea88b1c32ec3
Alex Verbruggen <verbruggenalex@gmail.com>
2019-06-14 05:53:24 +0000

    MULTISITE-21214: Swap ref for sha commit.

--- server/hook.go
+++ server/hook.go
@@ -169,8 +169,8 @@ func PostHook(c *gin.Context) {
 		// if failed, check if we have a fallback in the server config
 		if len(os.Getenv("DRONE_SERVER_YAML_FALLBACK")) > 0 {
 			// download it
-			resp, err := http.Get(fmt.Sprintf("%s?event=%s&repo=%s&branch=%s&refspec=%s&ref=%s",
-				os.Getenv("DRONE_SERVER_YAML_FALLBACK"), build.Event, repo.FullName, build.Branch, build.Refspec, build.Ref))
+			resp, err := http.Get(fmt.Sprintf("%s?event=%s&repo=%s&branch=%s&refspec=%s&sha=%s",
+				os.Getenv("DRONE_SERVER_YAML_FALLBACK"), build.Event, repo.FullName, build.Branch, build.Refspec, build.Commit))
 			if err != nil {
 				logrus.Errorf("failure to get build config for %s. %s", repo.FullName, err)
 				c.AbortWithError(404, err)
